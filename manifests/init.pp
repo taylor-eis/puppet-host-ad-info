ae2d9075e017172068945e3bd8078cbdc2b04c53
brgerig <brgerig@taylor.edu>
2023-02-03 10:03:46 -0500

    Create init.pp

---
+++ manifests/init.pp
@@ -0,0 +1,12 @@
+class host_ad_info {
+  file { '/usr/local/bin/host_ad_info.sh':
+    ensure => file,
+    source => 'puppet:///modules/host_ad_info/host_ad_info.sh'
+  }
+  cron { 'host_ad_info':
+    command => '/usr/local/bin/host_ad_info.sh',
+    user => 'root',
+    hour => 1,
+    minute => 0,
+  }
+}
